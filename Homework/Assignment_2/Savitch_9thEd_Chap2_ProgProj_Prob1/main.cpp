63b01b2c774d9adfa7fce7c20d77e2309513a857
bhitt <bhitt@student.rcc.edu>
2015-03-16 10:45:51 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Homework/Assignment_2/Savitch_9thEd_Chap2_ProgProj_Prob1/main.cpp
+++ Homework/Assignment_2/Savitch_9thEd_Chap2_ProgProj_Prob1/main.cpp
@@ -12,20 +12,29 @@ using namespace std;
 
 //Global Constants
 const int GPPCNV = 454;
+const float SWTCNV = 0.001;
 //Function Prototypes
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
     int uWght, soda=350, tGram;
-    float sSoda;
+    float lethdos, sSoda;
+    char ans;
     //Input
-    cout<<"How much do you weigh in lbs?"<<endl;
+    do{
+    cout<<"How much will your end weight be in lbs after you are done dieting?"<<endl;
     cin>>uWght;
     //Calculate
     tGram = uWght*GPPCNV;
+    lethdos = tGram/7;
+    sSoda = (lethdos/(SWTCNV*soda))-1;
     //Output
-    cout<<"You may drink "<< sSoda <<" diet soda without dying"<<endl;
+    cout<<"You may drink "<< sSoda <<" cans of diet soda without dying"<<endl;
+    cout<<"Would you like to run this program again?"<<endl;
+    cout<<"Type Y for yes, or N for no. Then hit return"<<endl;
+    cin>>ans;
+    } while (ans=='Y'||ans=='y');
     cin.get();
     //Exit Stage Right
     return 0;
