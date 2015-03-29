b400fe2f5d05f32fa161622dc489c0de86ba7c36
Bhitt <bhitt@student.rcc.edu>
2015-03-28 23:05:37 -0700

    Signed-off-by: Bhitt <bhitt@student.rcc.edu>

---
+++ Homework/Assignment_3/Savitch_9thEd_Chap3_PracProg_Prob5/main.cpp
@@ -0,0 +1,34 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Branden Hitt
+ * Created on March 28, 2015, 10:50 PM
+ *      Purpose: Output the prime numbers from 1 to 100
+ */
+
+//System Libraries
+#include <iostream>//I/O standard
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    short p,j;
+    //Prompt user
+    cout<<"This program finds and outputs the prime numbers from 1 to 100"<<endl;
+    //Calculate
+    for (p=2;p<=100;p++)
+        for (j=2;j<p;j++){
+            if(p%j==0)
+                break;
+            else if (p==j+1)
+                cout<<p<<" ";
+        }
+    return 0;
+}
