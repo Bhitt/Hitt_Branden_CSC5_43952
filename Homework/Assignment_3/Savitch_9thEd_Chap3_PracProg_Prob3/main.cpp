1fdf7bbcf1cdbe35129e4be959c0313c525bb66a
Bhitt <bhitt@student.rcc.edu>
2015-03-28 20:16:44 -0700

    Signed-off-by: Bhitt <bhitt@student.rcc.edu>

--- Homework/Assignment_3/Savitch_9thEd_Chap3_PracProg_Prob3/main.cpp
+++ Homework/Assignment_3/Savitch_9thEd_Chap3_PracProg_Prob3/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>//I/O standard
+#include <string>
 using namespace std;
 
 //User Libraries
@@ -18,132 +19,205 @@ using namespace std;
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
-   int bMonth,bDay;
+   int bMonth,bDay; // birth month and day
+   string cap, aqu, pisc, arie, taur, gem, canc, leo, virgo, libra, scorpio, sag; //horoscopes
+   cap="What is dead may never die.";// Capricorn 
+   aqu="You will encounter a white walker soon.";// Aquarius
+   pisc="All men must die.";// Pisces
+   arie="A Lannister always pays his debt.";// Aries
+   taur="You will encounter a giant named Hodor";// Taurus
+   gem="You will find that John Snow knows nothing";// Gemini
+   canc="You will light the biggest fire the North has ever seen";// Cancer
+   leo="You will change your name to Wreak";// Leo
+   virgo="You will have your face crushed by The Mountain";// Virgo
+   libra="All dwarves are bastards in their father's eyes.";// Libra
+   scorpio="Don't drink the wine.";// Scorpio
+   sag="Winter is Coming.";// Sagittarius
     
     //Prompt the user for inputs
     cout<<"What month is your Birthday in? For Jan type 1, for Feb type 2, etc."<<endl;
-    cin>>bMonth;
+    cin>>bMonth;// user's birthday month
     cout<<"What day of the month is your Birthday (xx)"<<endl;
-    cin>>bDay;
+    cin>>bDay;// user's birthday day
     //Output 
     switch(bMonth){
-        case 1: {
-            if(bDay<20){
+        case 1: {// January
+            if(bDay<19){// birthday before 19th
                 cout<<"Your sign is Capricorn and your horoscope is :"<<endl;
-                cout<<"What is dead may never die."<<endl;
-            }else {
-                cout<<"Your sign is Aquarius and your horoscope is :"<<endl;
-                cout<<"You will encounter a white walker soon."<<endl;
-            }
-            break;
-        }
-        case 2:{
-            if(bDay<19){
-                cout<<"Your sign is Aquarius and your horoscope is :"<<endl;
-                cout<<"You will encounter a white walker soon."<<endl;
-            }else{
-                cout<<"Your sign is Pisces and your horoscope is :"<<endl;
-                cout<<"All men must die."<<endl;
-            }
-            break;
-        }
-        case 3: {
-            if(bDay<21){
-                cout<<"Your sign is Pisces and your horoscope is :"<<endl;
-                cout<<"All men must die."<<endl;
-            }else{
-                cout<<"Your sign is Aries and your horoscope is :"<<endl;
-                cout<<"A Lannister always pays his debt."<<endl;
-            }
-            break;
-        }
-        case 4:{
-            if(bDay<20){
-                cout<<"Your sign is Aries and your horoscope is :"<<endl;
-                cout<<"A Lannister always pays his debt"<<endl;
-            }else{
-                cout<<"Your sign is Taurus and your horoscope is :"<<endl;
-                cout<<"You will encounter a giant named Hodor"<<endl;
-            }
-            break;
-        }
-        case 5:{
-            if(bDay<22){
-                cout<<"Your sign is Taurus and your horoscope is :"<<endl;
-                cout<<"You will encounter a giant named Hodor"<<endl;
-            }else{
-                cout<<"Your sign is Gemini and your horoscope is :"<<endl;
-                cout<<"You will find that John Snow knows nothing"<<endl;
-            }
-            break;
-        }
-         case 6:{
-            if(bDay<22){
-                cout<<"Your sign is Gemini and your horoscope is :"<<endl;
-                cout<<"You will find that John Snow knows nothing"<<endl;
-            }else{
-                cout<<"Your sign is Cancer and your horoscope is :"<<endl;
-                cout<<"You will light the biggest fire the North has ever seen"<<endl;
-            }
-            break;
-        }
-          case 7:{
-            if(bDay<23){
-                cout<<"Your sign is Cancer and your horoscope is :"<<endl;
-                cout<<"You will light the biggest fire the North has ever seen"<<endl;
-            }else{
-                cout<<"Your sign is Leo and your horoscope is :"<<endl;
-                cout<<"You will change your name to Wreak"<<endl;
-            }
-            break;
-        }
-           case 8:{
-            if(bDay<23){
-                cout<<"Your sign is Leo and your horoscope is :"<<endl;
-                cout<<"You will change your name to Wreak"<<endl;
-            }else{
-                cout<<"Your sign is Virgo and your horoscope is :"<<endl;
-                cout<<"You will have your face crushed by The Mountain"<<endl;
-            }
-            break;
-        }
-            case 9:{
-            if(bDay<23){
-                cout<<"Your sign is Virgo and your horoscope is :"<<endl;
-                cout<<"You will have your face crushed by The Mountain"<<endl;
-            }else{
-                cout<<"Your sign is Libra and your horoscope is :"<<endl;
-                cout<<"All dwarves are bastards in their father's eyes."<<endl;
-            }
-            break;
-        }
-             case 10:{
-            if(bDay<23){
-                cout<<"Your sign is Libra and your horoscope is :"<<endl;
-                cout<<"All dwarves are bastards in their father's eyes."<<endl;
-            }else{
-                cout<<"Your sign is Scorpio and your horoscope is :"<<endl;
-                cout<<"Don't drink the wine."<<endl;
-            }
-            break;
-        }
-              case 11:{
-            if(bDay<22){
-                cout<<"Your sign is Scorpio and your horoscope is :"<<endl;
-                cout<<"Don't drink the wine"<<endl;
-            }else{
-                cout<<"Your sign is Sagittarius and your horoscope is :"<<endl;
-                cout<<"Winter is Coming."<<endl;
-            }
-            break;
-        }
-               case 12:{
-            if(bDay<22){
-                cout<<"Your sign is Sagittarius and your horoscope is :"<<endl;
-                cout<<"Winter is Coming."<<endl;
-            }else{
+                cout<<cap<<endl;
+            }else if (bDay==19){// birthday on 19th
                 cout<<"Your sign is Capricorn and your horoscope is :"<<endl;
-                cout<<"What is dead may never die."<<endl;
+                cout<<cap<<endl;
+                cout<<"You are on the cusp of being an Aquarius which has the horoscope: "<<endl;
+                cout<<aqu<<endl;
+            }else {// birthday after 19th
+                cout<<"Your sign is Aquarius and your horoscope is :"<<endl;
+                cout<<aqu<<endl;
+            }
+            break;
+        }
+        case 2:{// Febuary
+            if(bDay<18){// birthday before 18th
+                cout<<"Your sign is Aquarius and your horoscope is :"<<endl;
+                cout<<aqu<<endl;
+            }else if (bDay==18){// birthday on 18th
+                cout<<"Your sign is Aquarius and your horoscope is :"<<endl;
+                cout<<aqu<<endl;
+                cout<<"You are on the cusp of being a Pisces which has the horoscope: "<<endl;
+                cout<<pisc<<endl;
+            }else{// birthday after 18th
+                cout<<"Your sign is Pisces and your horoscope is :"<<endl;
+                cout<<pisc<<endl;
+            }
+            break;
+        }
+        case 3: {// March
+            if(bDay<20){// birthday before 20th
+                cout<<"Your sign is Pisces and your horoscope is :"<<endl;
+                cout<<pisc<<endl;
+            }else if (bDay==20){// birthday on 20th
+                cout<<"Your sign is Pisces and your horoscope is :"<<endl;
+                cout<<pisc<<endl;
+                cout<<"You are on the cusp of being an Aries which has the horoscope: "<<endl;
+                cout<<arie<<endl;
+            }else{// birthday after 20th
+                cout<<"Your sign is Aries and your horoscope is :"<<endl;
+                cout<<arie<<endl;
+            }
+            break;
+        }
+        case 4:{// April
+            if(bDay<19){// birthday before 19th
+                cout<<"Your sign is Aries and your horoscope is :"<<endl;
+                cout<<arie<<endl;
+            }else if (bDay==19){// birthday on 19th
+                cout<<"Your sign is Aries and your horoscope is :"<<endl;
+                cout<<arie<<endl;
+                cout<<"You are on the cusp of being a Taurus which has the horoscope: "<<endl;
+                cout<<taur<<endl;
+            }else{// birthday after 19th
+                cout<<"Your sign is Taurus and your horoscope is :"<<endl;
+                cout<<taur<<endl;
+            }
+            break;
+        }
+        case 5:{// May
+            if(bDay<20){// birthday before 20th
+                cout<<"Your sign is Taurus and your horoscope is :"<<endl;
+                cout<<taur<<endl;
+            }else if (bDay==20){// birthday on 20th
+                cout<<"Your sign is Taurus and your horoscope is :"<<endl;
+                cout<<taur<<endl;
+                cout<<"You are on the cusp of being a Gemini which has the horoscope: "<<endl;
+                cout<<gem<<endl;
+            }else{// birthday after 20th
+                cout<<"Your sign is Gemini and your horoscope is :"<<endl;
+                cout<<gem<<endl;
+            }
+            break;
+        }
+         case 6:{// June
+            if(bDay<21){// birthday before 21st
+                cout<<"Your sign is Gemini and your horoscope is :"<<endl;
+                cout<<gem<<endl;
+            }else if (bDay==21){// birthday on 21st
+                cout<<"Your sign is Gemini and your horoscope is :"<<endl;
+                cout<<gem<<endl;
+                cout<<"You are on the cusp of being a Cancer which has the horoscope: "<<endl;
+                cout<<canc<<endl;
+            }else{// birthday after 21st
+                cout<<"Your sign is Cancer and your horoscope is :"<<endl;
+                cout<<canc<<endl;
+            }
+            break;
+        }
+          case 7:{// July
+            if(bDay<22){// birthday before 22nd
+                cout<<"Your sign is Cancer and your horoscope is :"<<endl;
+                cout<<canc<<endl;
+            }else if (bDay==22){// birthday on 22nd
+                cout<<"Your sign is Cancer and your horoscope is :"<<endl;
+                cout<<canc<<endl;
+                cout<<"You are on the cusp of being a Leo which has the horoscope: "<<endl;
+                cout<<leo<<endl;
+            }else{// birthday after 22nd
+                cout<<"Your sign is Leo and your horoscope is :"<<endl;
+                cout<<leo<<endl;
+            }
+            break;
+        }
+           case 8:{// August
+            if(bDay<22){// birthday before 22nd
+                cout<<"Your sign is Leo and your horoscope is :"<<endl;
+                cout<<leo<<endl;
+            }else if (bDay==22){// birthday on 22
+                cout<<"Your sign is Leo and your horoscope is :"<<endl;
+                cout<<leo<<endl;
+                cout<<"You are on the cusp of being a Virgo which has the horoscope: "<<endl;
+                cout<<virgo<<endl;
+            }else{// birthday after 22nd
+                cout<<"Your sign is Virgo and your horoscope is :"<<endl;
+                cout<<virgo<<endl;
+            }
+            break;
+        }
+            case 9:{// September
+            if(bDay<22){// birthday before 22nd
+                cout<<"Your sign is Virgo and your horoscope is :"<<endl;
+                cout<<virgo<<endl;
+            }else if (bDay==22){// birthday on 22nd
+                cout<<"Your sign is Virgo and your horoscope is :"<<endl;
+                cout<<virgo<<endl;
+                cout<<"You are on the cusp of being a Libra which has the horoscope: "<<endl;
+                cout<<libra<<endl;
+            }else{// birthday after 22nd
+                cout<<"Your sign is Libra and your horoscope is :"<<endl;
+                cout<<libra<<endl;
+            }
+            break;
+        }
+            case 10:{// October
+            if(bDay<22){// birthday before 22nd
+                cout<<"Your sign is Libra and your horoscope is :"<<endl;
+                cout<<libra<<endl;
+            }else if (bDay==22){// birthday on 22nd
+                cout<<"Your sign is Libra and your horoscope is :"<<endl;
+                cout<<libra<<endl;
+                cout<<"You are on the cusp of being a Scorpio which has the horoscope: "<<endl;
+                cout<<scorpio<<endl;
+            }else{// birthday after 22nd
+                cout<<"Your sign is Scorpio and your horoscope is :"<<endl;
+                cout<<scorpio<<endl;
+            }
+            break;
+        }
+            case 11:{// November
+            if(bDay<21){// birthday before 21st
+                cout<<"Your sign is Scorpio and your horoscope is :"<<endl;
+                cout<<scorpio<<endl;
+            }else if (bDay==21){// birthday on 21st
+                cout<<"Your sign is Scorpio and your horoscope is :"<<endl;
+                cout<<scorpio<<endl;
+                cout<<"You are on the cusp of being a Sagittarius which has the horoscope: "<<endl;
+                cout<<sag<<endl;
+            }else{// birthday after 21st
+                cout<<"Your sign is Sagittarius and your horoscope is :"<<endl;
+                cout<<sag<<endl;
+            }
+            break;
+        }
+            case 12:{// December
+            if(bDay<21){// birthday before 21st
+                cout<<"Your sign is Sagittarius and your horoscope is :"<<endl;
+                cout<<sag<<endl;
+            }else if (bDay==21){// birthday on 21st
+                cout<<"Your sign is Sagittarius and your horoscope is :"<<endl;
+                cout<<sag<<endl;
+                cout<<"You are on the cusp of being a Capricorn which has the horoscope: "<<endl;
+                cout<<cap<<endl;
+            }else{// birthday after 21st
+                cout<<"Your sign is Capricorn and your horoscope is :"<<endl;
+                cout<<cap<<endl;
             }
             break;
         }
