630623df44459af3278881d7211d530ca6657cbe
bhitt <bhitt@student.rcc.edu>
2015-03-30 10:53:52 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Homework/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob13/main.cpp
+++ Homework/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob13/main.cpp
@@ -1,19 +1,41 @@
 /* 
  * File:   main.cpp
- * Author: Administrator
- *
- * Created on March 29, 2015, 9:59 PM
+ * Author: Branden Hitt
+ * Created on March 25, 2015, 10:10 AM
+ *      Purpose: Solve the Riddler's Riddle
+ *              Find a 4 digit number that passes the conditions
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>//I/O standard
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    //Loop for all possible digits
+    for(int d1000=0;d1000<=9;d1000++){//loops thousand digit
+        for(int d100=0;d100<=9;d100++){//loops hundred digit
+            for(int d10=0;d10<=9;d10++){//loops tens digit
+                for(int d1=0;d1<=9;d1++){//loops ones digit
+                    if((d1000==3*d10) &&//condition 1 (thousand must be 3 times tens)
+                        (d1%2==1)   &&//condition 2 (must be odd)
+                        (d1000+d100+d10+d1==27) && //condition 3 (sum of digits must be 27)
+                           (d1!=d10&&d1!=100&&d1!=d1000&&//condition 4 (digits must be different)
+                            d10!=d100&&d10!=1000&&
+                            d100!=1000)){
+                        cout<<"The Address Number = ";
+                        cout<<d1000<<d100<<d10<<d1<<endl;
+                    }
+                }
+        }
+    }
+    }
     return 0;
 }
-
--- Homework/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob4/main.cpp
+++ Homework/Assignment_3/Savitch_9thEd_Chap3_ProgProj_Prob4/main.cpp
@@ -19,30 +19,435 @@ using namespace std;
 int main(int argc, char** argv) {
     //Declare Variables
     short numCard,totVal=0;
-    char card1,card2,card3,card4;
+    char card1,card2,card3,card4,card5;
     //Prompt the user for inputs
     cout<<"How many cards do you have: (2,3,4,5)"<<endl;
     cin>>numCard;
     switch (numCard){//checks for amount of cards in hand
-        case 2:
+        case 2://2 cards in hand
+            //prompt user for card values
             cout<<"Please enter the card values.(2-9 or T,J,Q,K,A):"<<endl;
             cout<<"What is the value of the first card?"<<endl;
             cin>>card1;
             cout<<"What is the value of the second card?"<<endl;
             cin>>card2;
+            //check card 1 for value
             if (card1=='t' || card1=='T' || card1=='j' || card1=='J' || 
-                    card1=='q' || card1=='Q'|| card1=='k' || card1=='K' || card1=='a'){
+                    card1=='q' || card1=='Q'|| card1=='k' || card1=='K'){
+                totVal+=10;
+            }else if(card1=='a'||card1=='A'){
+                totVal+=11;
+            }else if(card1=='2'){
                 totVal+=2;
-            }else {
-                totVal+=card1;
+            }else if(card1=='3'){
+                totVal+=3;
+            }else if(card1=='4'){
+                totVal+=4;
+            }else if(card1=='5'){
+                totVal+=5;
+            }else if(card1=='6'){
+                totVal+=6;
+            }else if(card1=='7'){
+                totVal+=7;
+            }else if(card1=='8'){
+                totVal+=8;
+            }else if(card1=='9'){
+                totVal+=9;
+            }else{
+                cout<<"Invalid input"<<endl;
             }
-            totVal+=card2;
-            cout<<"Your total value is "<<totVal<<endl;
+            //check card 2 for value
+            if (card2=='t' || card2=='T' || card2=='j' || card2=='J' || 
+                    card2=='q' || card2=='Q'|| card2=='k' || card2=='K'){
+                totVal+=10;
+            }else if(card2=='a'||card2=='A'){
+                totVal+=11;
+            }else if(card2=='2'){
+                totVal+=2;
+            }else if(card2=='3'){
+                totVal+=3;
+            }else if(card2=='4'){
+                totVal+=4;
+            }else if(card2=='5'){
+                totVal+=5;
+            }else if(card2=='6'){
+                totVal+=6;
+            }else if(card2=='7'){
+                totVal+=7;
+            }else if(card2=='8'){
+                totVal+=8;
+            }else if(card2=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            if(totVal<22){//if the total score is less than 22
+                cout<<"Your total score is "<<totVal<<endl;
+            }else{//if the score is 22 or more
+                if(card1=='a'||card1=='A'){//check card 1 for an ace
+                    totVal-=10;
+                    if(totVal<22){
+                        cout<<"Your total score is "<<totVal<<endl;
+                    }else if(card2=='a'||card2=='A'){
+                        totVal-=10;
+                    }else{
+                        cout<<"Busted! Your total was"<<totVal<<endl;
+                    }
+                }else if(card2=='a'){//check card 2 for ace
+                }else{
+                cout<<"Busted! Your total was "<<totVal<<endl;
+                }
+            }
             break;
-        case 3:
-        case 4:
-        case 5:
-        default:
+        case 3://3 cards in hand
+            cout<<"Please enter the card values.(2-9 or T,J,Q,K,A):"<<endl;
+            cout<<"What is the value of the first card?"<<endl;
+            cin>>card1;
+            cout<<"What is the value of the second card?"<<endl;
+            cin>>card2;
+            cout<<"What is the value of the third card?"<<endl;
+            cin>>card3;
+            //check card 1 for value
+            if (card1=='t' || card1=='T' || card1=='j' || card1=='J' || 
+                    card1=='q' || card1=='Q'|| card1=='k' || card1=='K'){
+                totVal+=10;
+            }else if(card1=='a'||card1=='A'){
+                totVal+=11;
+            }else if(card1=='2'){
+                totVal+=2;
+            }else if(card1=='3'){
+                totVal+=3;
+            }else if(card1=='4'){
+                totVal+=4;
+            }else if(card1=='5'){
+                totVal+=5;
+            }else if(card1=='6'){
+                totVal+=6;
+            }else if(card1=='7'){
+                totVal+=7;
+            }else if(card1=='8'){
+                totVal+=8;
+            }else if(card1=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 2 for value
+            if (card2=='t' || card2=='T' || card2=='j' || card2=='J' || 
+                    card2=='q' || card2=='Q'|| card2=='k' || card2=='K'){
+                totVal+=10;
+            }else if(card2=='a'||card2=='A'){
+                totVal+=11;
+            }else if(card2=='2'){
+                totVal+=2;
+            }else if(card2=='3'){
+                totVal+=3;
+            }else if(card2=='4'){
+                totVal+=4;
+            }else if(card2=='5'){
+                totVal+=5;
+            }else if(card2=='6'){
+                totVal+=6;
+            }else if(card2=='7'){
+                totVal+=7;
+            }else if(card2=='8'){
+                totVal+=8;
+            }else if(card2=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 3 for value
+            if (card3=='t' || card3=='T' || card3=='j' || card3=='J' || 
+                    card3=='q' || card3=='Q'|| card3=='k' || card3=='K'){
+                totVal+=10;
+            }else if(card3=='a'||card3=='A'){
+                totVal+=11;
+            }else if(card3=='2'){
+                totVal+=2;
+            }else if(card3=='3'){
+                totVal+=3;
+            }else if(card3=='4'){
+                totVal+=4;
+            }else if(card3=='5'){
+                totVal+=5;
+            }else if(card3=='6'){
+                totVal+=6;
+            }else if(card3=='7'){
+                totVal+=7;
+            }else if(card3=='8'){
+                totVal+=8;
+            }else if(card3=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            if(totVal<22){
+                cout<<"Your total score is "<<totVal<<endl;
+            }else{
+                cout<<"Busted! Your total was "<<totVal<<endl;
+            }
+            break;
+        case 4://4 cards in hand
+            cout<<"Please enter the card values.(2-9 or T,J,Q,K,A):"<<endl;
+            cout<<"What is the value of the first card?"<<endl;
+            cin>>card1;
+            cout<<"What is the value of the second card?"<<endl;
+            cin>>card2;
+            cout<<"What is the value of the third card?"<<endl;
+            cin>>card3;
+            cout<<"What is the value of the fourth card?"<<endl;
+            cin>>card4;
+            //check card 1 for value
+            if (card1=='t' || card1=='T' || card1=='j' || card1=='J' || 
+                    card1=='q' || card1=='Q'|| card1=='k' || card1=='K'){
+                totVal+=10;
+            }else if(card1=='a'||card1=='A'){
+                totVal+=11;
+            }else if(card1=='2'){
+                totVal+=2;
+            }else if(card1=='3'){
+                totVal+=3;
+            }else if(card1=='4'){
+                totVal+=4;
+            }else if(card1=='5'){
+                totVal+=5;
+            }else if(card1=='6'){
+                totVal+=6;
+            }else if(card1=='7'){
+                totVal+=7;
+            }else if(card1=='8'){
+                totVal+=8;
+            }else if(card1=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 2 for value
+            if (card2=='t' || card2=='T' || card2=='j' || card2=='J' || 
+                    card2=='q' || card2=='Q'|| card2=='k' || card2=='K'){
+                totVal+=10;
+            }else if(card2=='a'||card2=='A'){
+                totVal+=11;
+            }else if(card2=='2'){
+                totVal+=2;
+            }else if(card2=='3'){
+                totVal+=3;
+            }else if(card2=='4'){
+                totVal+=4;
+            }else if(card2=='5'){
+                totVal+=5;
+            }else if(card2=='6'){
+                totVal+=6;
+            }else if(card2=='7'){
+                totVal+=7;
+            }else if(card2=='8'){
+                totVal+=8;
+            }else if(card2=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 3 for value
+            if (card3=='t' || card3=='T' || card3=='j' || card3=='J' || 
+                    card3=='q' || card3=='Q'|| card3=='k' || card3=='K'){
+                totVal+=10;
+            }else if(card3=='a'||card3=='A'){
+                totVal+=11;
+            }else if(card3=='2'){
+                totVal+=2;
+            }else if(card3=='3'){
+                totVal+=3;
+            }else if(card3=='4'){
+                totVal+=4;
+            }else if(card3=='5'){
+                totVal+=5;
+            }else if(card3=='6'){
+                totVal+=6;
+            }else if(card3=='7'){
+                totVal+=7;
+            }else if(card3=='8'){
+                totVal+=8;
+            }else if(card3=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 4 for value
+            if (card4=='t' || card4=='T' || card4=='j' || card4=='J' || 
+                    card4=='q' || card4=='Q'|| card4=='k' || card4=='K'){
+                totVal+=10;
+            }else if(card4=='a'||card4=='A'){
+                totVal+=11;
+            }else if(card4=='2'){
+                totVal+=2;
+            }else if(card4=='3'){
+                totVal+=3;
+            }else if(card4=='4'){
+                totVal+=4;
+            }else if(card4=='5'){
+                totVal+=5;
+            }else if(card4=='6'){
+                totVal+=6;
+            }else if(card4=='7'){
+                totVal+=7;
+            }else if(card4=='8'){
+                totVal+=8;
+            }else if(card4=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            if(totVal<22){
+                cout<<"Your total score is "<<totVal<<endl;
+            }else{
+                cout<<"Busted! Your total was "<<totVal<<endl;
+            }
+            break;
+        case 5://5 cards in hand
+            cout<<"Please enter the card values.(2-9 or T,J,Q,K,A):"<<endl;
+            cout<<"What is the value of the first card?"<<endl;
+            cin>>card1;
+            cout<<"What is the value of the second card?"<<endl;
+            cin>>card2;
+            cout<<"What is the value of the third card?"<<endl;
+            cin>>card3;
+            cout<<"What is the value of the fourth card?"<<endl;
+            cin>>card4;
+            cout<<"What is the value of the fifth card?"<<endl;
+            cin>>card5;
+            //check card 1 for value
+            if (card1=='t' || card1=='T' || card1=='j' || card1=='J' || 
+                    card1=='q' || card1=='Q'|| card1=='k' || card1=='K'){
+                totVal+=10;
+            }else if(card1=='a'||card1=='A'){
+                totVal+=11;
+            }else if(card1=='2'){
+                totVal+=2;
+            }else if(card1=='3'){
+                totVal+=3;
+            }else if(card1=='4'){
+                totVal+=4;
+            }else if(card1=='5'){
+                totVal+=5;
+            }else if(card1=='6'){
+                totVal+=6;
+            }else if(card1=='7'){
+                totVal+=7;
+            }else if(card1=='8'){
+                totVal+=8;
+            }else if(card1=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 2 for value
+            if (card2=='t' || card2=='T' || card2=='j' || card2=='J' || 
+                    card2=='q' || card2=='Q'|| card2=='k' || card2=='K'){
+                totVal+=10;
+            }else if(card2=='a'||card2=='A'){
+                totVal+=11;
+            }else if(card2=='2'){
+                totVal+=2;
+            }else if(card2=='3'){
+                totVal+=3;
+            }else if(card2=='4'){
+                totVal+=4;
+            }else if(card2=='5'){
+                totVal+=5;
+            }else if(card2=='6'){
+                totVal+=6;
+            }else if(card2=='7'){
+                totVal+=7;
+            }else if(card2=='8'){
+                totVal+=8;
+            }else if(card2=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 3 for value
+            if (card3=='t' || card3=='T' || card3=='j' || card3=='J' || 
+                    card3=='q' || card3=='Q'|| card3=='k' || card3=='K'){
+                totVal+=10;
+            }else if(card3=='a'||card3=='A'){
+                totVal+=11;
+            }else if(card3=='2'){
+                totVal+=2;
+            }else if(card3=='3'){
+                totVal+=3;
+            }else if(card3=='4'){
+                totVal+=4;
+            }else if(card3=='5'){
+                totVal+=5;
+            }else if(card3=='6'){
+                totVal+=6;
+            }else if(card3=='7'){
+                totVal+=7;
+            }else if(card3=='8'){
+                totVal+=8;
+            }else if(card3=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 4 for value
+            if (card4=='t' || card4=='T' || card4=='j' || card4=='J' || 
+                    card4=='q' || card4=='Q'|| card4=='k' || card4=='K'){
+                totVal+=10;
+            }else if(card4=='a'||card4=='A'){
+                totVal+=11;
+            }else if(card4=='2'){
+                totVal+=2;
+            }else if(card4=='3'){
+                totVal+=3;
+            }else if(card4=='4'){
+                totVal+=4;
+            }else if(card4=='5'){
+                totVal+=5;
+            }else if(card4=='6'){
+                totVal+=6;
+            }else if(card4=='7'){
+                totVal+=7;
+            }else if(card4=='8'){
+                totVal+=8;
+            }else if(card4=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+            //check card 5 for value
+            if (card5=='t' || card5=='T' || card5=='j' || card5=='J' || 
+                    card5=='q' || card5=='Q'|| card5=='k' || card5=='K'){
+                totVal+=10;
+            }else if(card5=='a'||card5=='A'){
+                totVal+=11;
+            }else if(card5=='2'){
+                totVal+=2;
+            }else if(card5=='3'){
+                totVal+=3;
+            }else if(card5=='4'){
+                totVal+=4;
+            }else if(card5=='5'){
+                totVal+=5;
+            }else if(card5=='6'){
+                totVal+=6;
+            }else if(card5=='7'){
+                totVal+=7;
+            }else if(card5=='8'){
+                totVal+=8;
+            }else if(card5=='9'){
+                totVal+=9;
+            }else {
+                cout<<"Invalid input"<<endl;
+            }
+           if(totVal<22){
+                cout<<"Your total score is "<<totVal<<endl;
+            }else{
+                cout<<"Busted! Your total was "<<totVal<<endl;
+            }
+            break;
+        default://Invalid input
             cout<<"Invalid entry"<<endl;
     }
     
