db46529d5d0e4df9c4345aba3de8525715c1b83c
Bhitt <bhitt@student.rcc.edu>
2015-04-08 16:32:08 -0700

    Signed-off-by: Bhitt <bhitt@student.rcc.edu>

--- Homework/Assignment_4/Savitch_9thEd_Chap4_ProgProj_Prob9/main.cpp
+++ Homework/Assignment_4/Savitch_9thEd_Chap4_ProgProj_Prob9/main.cpp
@@ -19,36 +19,47 @@ unsigned short draft(int team);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variable
-    int team, pick, pick2, pick3, pick4, tPcked=0;
+    int a, pick, pick2=0, pick3, pick4, tPcked=0;
     srand(static_cast<unsigned int>(time(0)));
     //Output
-    pick=draft(team);
+    cout<<"**********************************************************"<<endl;
+    cout<<"This program will find draft picks for the lowest ranking teams."<<endl;
+    cout<<"Balls with numbers on them will be taken from an urn to determine pick order."<<endl;
+    cout<<"Team 1 will have 20 balls (1-20) in the urn for coming in last."<<endl;
+    cout<<"Team 2 will have 10 balls (21-30) in the urn for coming in second to last."<<endl;
+    cout<<"Team 3 will have 6 balls  (31-36) in the urn for coming in third-to last."<<endl;
+    cout<<"Team 4 will have 4 balls  (37-40) in the urn for coming in fourth-to-last."<<endl;
+    cout<<"**********************************************************"<<endl;
+    pick=draft(a);
     tPcked+=1;
-    cout<<"The first pick is Team "<<pick<<endl;
-    pick2=draft(team);
-    cout<<"The second pick is Team "<<pick<<endl;
-    pick3=draft(team);
-    cout<<"The third pick is Team "<<pick<<endl;
-    pick4=draft(team);
-    cout<<"The fourth pick is Team "<<pick<<endl;
+    cout<<"The First pick is Team "<<pick<<endl;
+    cout<<"**********************************************************"<<endl;
+    do{
+        pick2=draft(a);
+    }while(pick2==pick);
+    cout<<"The Second pick is Team "<<pick2<<endl;
+    cout<<"**********************************************************"<<endl;
+    do{
+        pick3=draft(a);
+    }while(pick3==pick||pick3==pick2);
+    cout<<"The Third pick is Team "<<pick3<<endl;
+    cout<<"**********************************************************"<<endl;
+    do{
+        pick4=draft(a);
+    }while(pick4==pick||pick4==pick2||pick4==pick3);
+    cout<<"The Fourth pick is Team "<<pick4<<endl;
     return 0;
 }
-unsigned short draft(int team){
-    int repeat=1;
-    do{
-    int team1=0, team2=0, team3=0, team4=0; 
+unsigned short draft(int a){
     int choice=(rand()%40)+1;
-    cout<<"choice:"<<choice<<endl;
-    if ((choice<21)&&(team1=0)){
+    cout<<"The Ball picked from the Urn is Ball:"<<choice<<endl;
+    if (choice<21){
         return 1;
-    }else if ((choice>=20)&&(choice<31)&&(team2=0)){
+    }else if ((choice>=20)&&(choice<31)){
         return 2;
-    }else if ((choice>=31)&&(choice<37)&&(team3=0)){
+    }else if ((choice>=31)&&(choice<37)){
         return 3;
-    }else if ((choice>=37)&&(choice<41)&&(team4=0)){
+    }else {
         return 4;
-    }else{
-        repeat=0;
     }
-    }while (repeat==0);
 }
