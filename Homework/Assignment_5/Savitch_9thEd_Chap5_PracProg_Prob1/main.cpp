4842de11e878ed8d8ed82d1468710cb8d9748ca0
Bhitt <bhitt@student.rcc.edu>
2015-04-16 21:59:22 -0700

    Signed-off-by: Bhitt <bhitt@student.rcc.edu>

---
+++ Homework/Assignment_5/Savitch_9thEd_Chap5_PracProg_Prob1/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Branden Hitt
+ * Created on April 16, 2015, 7:10 PM
+ *      Purpose: write a function that finds the average and standard deviation
+ *              of 4 scores
+ */
+
+//System Libraries
+#include <iostream>//I/O standard
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+short scores(short a, short b, short c, short d, short e, short f);
+short average();
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+   
+    //Prompt the user for inputs
+    
+    //Output 
+    
+    return 0;
+}
+short scores(short a, short b, short c, short d, short e, short f){
+    
+}
+short average(short a, short b, short c, short d){
+    int avg;
+    avg=(a+b+c+d)/4;
+    return avg;
+}
+float stdDev(){
+    
+}
