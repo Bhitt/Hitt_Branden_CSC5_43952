d517f82dc7300669540d25e77ca340131ca771d8
bhitt <bhitt@student.rcc.edu>
2015-04-22 08:50:04 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Homework/Assignment_5/Savitch_9thEd_Chap5_PracProg_Prob1/main.cpp
+++ Homework/Assignment_5/Savitch_9thEd_Chap5_PracProg_Prob1/main.cpp
@@ -7,7 +7,8 @@
  */
 
 //System Libraries
-#include <iostream>//I/O standard
+#include<iostream>
+#include<cmath>
 using namespace std;
 
 //User Libraries
@@ -15,26 +16,49 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-short scores(short a, short b, short c, short d, short e, short f);
-short average();
+float calc(short, short, short, short, float, float);
+float avg(short, short, short, short);
+float stdDev(short, short, short, short, float);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
-   
+    char cho;
+    do{
+    short s1,s2,s3,s4;
+    float av,stDev,fin;
     //Prompt the user for inputs
+    cout<<"This program finds the standard deviation of four scores"<<endl;
+    cout<<"Please input score 1:"<<endl;
+    cin>>s1;
+    cout<<"Please input score 2:"<<endl;
+    cin>>s2;
+    cout<<"Please input score 3:"<<endl;
+    cin>>s3;
+    cout<<"Please input score 4:"<<endl;
+    cin>>s4;
+    //Call functions
+    fin=calc(s1,s2,s3,s4,av,stDev);
     
     //Output 
-    
+    cout<<"The standard deviation is "<<fin<<endl;
+    cout<<"Would you like to test again?"<<endl;
+    cout<<"Enter in Y for yes or N for no:"<<endl;
+    cin>>cho;
+    }while(cho=='Y'||cho=='y');
     return 0;
 }
-short scores(short a, short b, short c, short d, short e, short f){
-    
+float calc(short a, short b, short c, short d, float x, float y){
+    x=avg(a,b,c,d);
+    y=stdDev(a,b,c,d,x);
+    return y;
 }
-short average(short a, short b, short c, short d){
-    int avg;
+float avg(short a, short b, short c, short d){
+    float avg;
     avg=(a+b+c+d)/4;
     return avg;
 }
-float stdDev(){
-    
+float stdDev(short a, short b, short c, short d, float avg){
+    float end;
+    end=sqrt(((pow((a-avg),2))+pow((b-avg),2)+pow((c-avg),2)+pow((d-avg),2))/4);
+    return end;
 }
