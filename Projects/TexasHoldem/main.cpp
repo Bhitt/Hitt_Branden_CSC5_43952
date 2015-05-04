71ed6c583ee26d60f9225bc6ed1a65a620bab406
bhitt <bhitt@student.rcc.edu>
2015-05-04 10:57:01 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

---
+++ Projects/TexasHoldem/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Branden Hitt
+ * Created on May 4, 2015, 10:40 AM
+ *      Purpose: Code a game of texas hold'em
+ */
+
+//System Libraries
+#include <iostream>//I/O standard
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+   
+    //Prompt the user for inputs
+    
+    //Output 
+    
+    return 0;
+}
