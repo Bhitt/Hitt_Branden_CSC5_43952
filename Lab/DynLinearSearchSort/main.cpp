c0b602f9ced6fb264cf4d893743eb604a73c9ba8
bhitt <bhitt@student.rcc.edu>
2015-05-18 09:16:37 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Lab/DynLinearSearchSort/main.cpp
+++ Lab/DynLinearSearchSort/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Branden Hitt
- * Created on May 6, 2015, 9:01 AM
- * Purpose: Example Linear Search/Mark Sort
+ * Created on May 18, 2015, 8:01 AM
+ * Purpose: Example Linear Search/Marl Sort
  */
 
 //System Libraries
@@ -16,20 +16,19 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void filAray(int [],int);
-void prntAry(int [],int,int);
-int linSrch(int [],int,int);
-void markSrt(int [],int);
+int *filAray(int);
+void prntAry(int *,int,int);
+int linSrch(int *,int,int);
+void markSrt(int *,int);
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //set the random number seed
     srand(static_cast<unsigned int>(time(0)));
     //Declare Variables
-    const int SIZE=100;
-    int array[SIZE];
-    //fill the array
-    filAray(array,SIZE);
+    int SIZE=100;
+    //allocate and fill the dynamic array
+    int *array=filAray(SIZE);
     //print the array
     prntAry(array,SIZE,10);
     //sort the array
@@ -41,6 +40,8 @@ int main(int argc, char** argv) {
     int rndVal=rand()%90+10;
     cout<<rndVal<<" was found at position "
             <<linSrch(array,SIZE,rndVal)<<endl;
+    //De-allocate memory
+    delete []array;
     //Exit stage right
     return 0;
 }
@@ -71,9 +72,12 @@ void prntAry(int a[],int n, int perLine){
     }
 }
 
-void filAray(int a[],int n){
+int *filAray(int n){
+    //allocate memory
+    int *a=new int[n];
+    //fill array
     for(int i=0;i<n;i++){
         a[i]=rand()%90+10;//2 digit numbers
     }
+    return a;
 }
-
